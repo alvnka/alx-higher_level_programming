755dcb778431062e5d93a6af1832e8ef9ae88810
alvnka <alvoausto@yahoo.com>
2022-12-25 00:39:16 +0300

    corection for 5 and 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 9):
     for j in range(i+1, 10):
         if i != j:
             if i == 8 and j == 9:
-                print(f"{i}{j}".format(), end="")
+                print(f"{i}{j}".format(), end="\n")
             else:
                 print(f"{i}{j}".format(), end=", ")
