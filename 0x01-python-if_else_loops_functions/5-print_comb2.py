f6c78f76edffa396b27c0798f80193cc15d0570e
alvnka <alvoausto@yahoo.com>
2022-12-25 00:37:28 +0300

    correction for 5 & 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    if i > 0:
-        print(", ".format(), end="")
-    print(f"{i:02d}".format(), end="")
+    if i == 99:
+        print(f"{i:02d}".format(), end="")
+    else:
+        print(f"{i:02d}".format(), end=", ")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for i in range(0, 9):
     for j in range(i+1, 10):
         if i != j:
-            if j > 1:
-                print(f", ".format(), end="")
-            print(f"{i}{j}".format(), end="")
+            if i == 8 and j == 9:
+                print(f"{i}{j}".format(), end="")
+            else:
+                print(f"{i}{j}".format(), end=", ")
