fe136f52dcddf59166fc62ed79a59e2c94d7cd16
alvnka <alvoausto@yahoo.com>
2022-12-29 17:43:18 +0300

    number 4 corrected

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ import hidden_4
 funcs = dir(hidden_4)
 
 if __name__ == "__main__":
-	for f in funcs:
-		if '__' not in f:
-			print(f)
+    for f in funcs:
+        if '__' not in f:
+            print(f)
