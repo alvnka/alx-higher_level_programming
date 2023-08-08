dc6e1d47007c26df12c8d6c611b23fe4e05be132
alvnka <alvondu@gmail.com>
2023-08-08 17:02:45 +0300

    corrections to 5, 11 and 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,6 +2,8 @@
 
 def fizzbuzz():
     for number in range(1, 101):
-        output = "Fizz" if (number % 3 == 0) else "Buzz" if (
-            number % 5 == 0) else number
-        print("{} ".format(output), end= '')
+        output = "FizzBuzz" if (
+            number % 3 == 0 and number % 5 == 0) else "Buzz" if (
+            number % 5 == 0) else "Fizz" if (
+            number % 3 == 0) else number
+        print("{} ".format(output), end='')
