8db7535d42c96e5ae8e8ac29b1f1f59c2f501b59
alvnka <alvondu@gmail.com>
2023-08-10 20:06:03 +0300

    103.0

---
+++ 0x02-python-import_modules/caps_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if __import__ != "__main__":
+	for alphabet in range(65, 91):
+		print("{}".format(chr(alphabet)), end= '') if alphabet < 90 else print("{}".format(chr(alphabet)))
