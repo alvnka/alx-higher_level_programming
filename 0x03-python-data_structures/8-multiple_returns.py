e0251c4afc0f8b0097a8a9e46b6f1303b9b7f49b
alvnka <alvondu@gmail.com>
2023-08-12 14:46:41 +0300

    correction 8.1

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence != "":
-        for count, character in enumerate(sentence):
-            continue
+    if sentence:
+        return ((len(sentence), sentence[0]))
     else:
         return ((0, None))
-    return ((count + 1, sentence[0]))
