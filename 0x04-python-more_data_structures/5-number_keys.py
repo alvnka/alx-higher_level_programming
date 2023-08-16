873e132b29817a5fb4427bca22ca6c96f7daea12
alvnka <alvondu@gmail.com>
2023-08-16 17:05:23 +0300

    correction 0x04 5.1

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for number_of_keys, key in enumerate(a_dictionary):
-        continue
-    return number_of_keys + 1
+    if a_dictionary:
+        for number_of_keys, key in enumerate(a_dictionary):
+            continue
+        return number_of_keys + 1
+    else:
+        return 0
